bd3320b9b1b5176f26a02a82d0363c28a379f417
FelipeQuinones <fquiones@uc.cl>
2023-12-23 16:15:00 -0300

    changes 1

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
+import RPi.GPIO as GPIO
 from flask import Flask, send_file, render_template
-#from picamera import PiCamera
+from picamera import PiCamera
 from io import BytesIO
-#import RPi.GPIO as GPIO
 
-# Setup GPIO for motor control
-#GPIO.setmode(GPIO.BCM)
-motor1 = 23  # replace with your GPIO pin
-motor2 = 24  # replace with your GPIO pin
-#GPIO.setup(motor1, GPIO.OUT)
-#GPIO.setup(motor2, GPIO.OUT)
+GPIO.setwarnings(False)  # Disable GPIO warnings
+GPIO.setmode(GPIO.BCM)
+
+motor1 = 23
+motor2 = 24
+
+GPIO.setup(motor1, GPIO.OUT)
+GPIO.setup(motor2, GPIO.OUT)
 
 app = Flask(__name__)
-#camera = PiCamera()
+camera = PiCamera()
 
 @app.route('/')
 def index():
@@ -21,7 +23,7 @@ def index():
 def capture_image():
     # Capture image from camera
     image_stream = BytesIO()
-    #camera.capture(image_stream, 'jpeg')
+    camera.capture(image_stream, 'jpeg')
     image_stream.seek(0)
     return send_file(image_stream, mimetype='image/jpeg')
 
@@ -29,17 +31,14 @@ def capture_image():
 def move_car(direction):
     # Control car movement
     if direction == 'forward':
-        #GPIO.output(motor1, GPIO.HIGH)
-        #GPIO.output(motor2, GPIO.LOW)
-        pass
+        GPIO.output(motor1, GPIO.HIGH)
+        GPIO.output(motor2, GPIO.LOW)
     elif direction == 'backward':
-        #GPIO.output(motor1, GPIO.LOW)
-        #GPIO.output(motor2, GPIO.HIGH)
-        pass
+        GPIO.output(motor1, GPIO.LOW)
+        GPIO.output(motor2, GPIO.HIGH)
     elif direction == 'stop':
-        #GPIO.output(motor1, GPIO.LOW)
-        #GPIO.output(motor2, GPIO.LOW)
-        pass
+        GPIO.output(motor1, GPIO.LOW)
+        GPIO.output(motor2, GPIO.LOW)
     else:
         return 'Invalid direction', 400
     return 'Car moved ' + direction, 200
@@ -48,5 +47,4 @@ if __name__ == '__main__':
     try:
         app.run(host='0.0.0.0', port=80)
     finally:
-        pass
-        #GPIO.cleanup()  # cleanup GPIO settings when the app is terminated
+        GPIO.cleanup()  # cleanup GPIO settings when the app is terminated
